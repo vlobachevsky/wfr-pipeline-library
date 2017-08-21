0d1f403666aeaf7146d317248ff3f64c16043ee0
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-08-21 17:59:25 +0300

    Initial implementation

--- vars/checkoutSVN.groovy
+++ vars/checkoutSVN.groovy
@@ -1,7 +1,17 @@
-#!groovy
+#!/usr/bin/env groovy
 
 // vars/checkoutSVN.groovy
 
-def call(String name = 'human') {
-    echo "Hello, ${name}."
+def call(String credentialsId, String url, String depthOption = 'infinity') {
+    checkout([
+      $class: 'SubversionSCM',
+      locations: [[
+        credentialsId: "$credentialsId",
+        depthOption: "$depthOption",
+        ignoreExternalsOption: true,
+        local: '.',
+        remote: "$url"
+      ]],
+      workspaceUpdater: [$class: 'UpdateWithRevertUpdater']
+    ])
 }
