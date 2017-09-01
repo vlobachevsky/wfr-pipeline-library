23c0f911e747b629a5e51a0410c352d55f1a21c6
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-01 17:25:59 +0300

    Implement buildMW

--- vars/checkoutSVN.groovy
+++ vars/checkoutSVN.groovy
@@ -2,14 +2,14 @@
 
 // vars/checkoutSVN.groovy
 
-def call(String credentialsId, String url, String depthOption = 'infinity') {
+def call(String credentialsId, String url, String localDir = '.', String depthOption = 'infinity') {
     checkout([
       $class: 'SubversionSCM',
       locations: [[
         credentialsId: "$credentialsId",
         depthOption: "$depthOption",
         ignoreExternalsOption: true,
-        local: '.',
+        local: "$localDir",
         remote: "$url"
       ]],
       workspaceUpdater: [$class: 'UpdateWithRevertUpdater']
