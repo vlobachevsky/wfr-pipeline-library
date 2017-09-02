ec1b88592b4148ae7877a5ec7e3373259b63bfc8
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-02 19:47:32 +0300

    Implement Build step in mw-build pipeline

--- vars/buildMW.groovy
+++ vars/buildMW.groovy
@@ -3,5 +3,23 @@
 def call() {
     bat 'ant BuildEclipseCompiler SetProperties'
     //powershell 'Write-Output "Hello, World!"'
+    powershell '''$path = $ENV:WORKSPACE
+        $year = Get-Date -Format yyyy
+        $month = Get-Date -Format MM
+
+        $text = "using System;
+        using System.Reflection;
+
+        [assembly: AssemblyVersion(`"6.52.$year.$month`")]
+        [assembly: AssemblyFileVersion(`"6.52.$year.$month`")]
+
+        [assembly: AssemblyCompany(`"Kronos Inc`")]
+        [assembly: AssemblyProduct(`"Kronos Workforce Ready`")]
+        [assembly: AssemblyCopyright(`"Copyright (c) 1996-$year Kronos Inc`")]
+        [assembly: CLSCompliant(true)]"
+
+        $text | Set-Content "$path\PunchMW\src\AccessControl\Global\GlobalAssemblyInfo.cs"
+    '''
+
     bat "\"${tool 'MSBuild-Default'}\" /p:Configuration=Release /p:Platform=\"Any CPU\" /t:Rebuild ${WORKSPACE}\\src\\AccessControl\\Build\\Build.xml"
 }
