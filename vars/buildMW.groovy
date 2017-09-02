cae76829e55e4e4b22c1144a4a3067e1b8ca40b7
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-02 18:43:56 +0300

    Implement Build step in mw-build pipeline

--- vars/buildMW.groovy
+++ vars/buildMW.groovy
@@ -3,5 +3,5 @@
 def call() {
     bat 'ant BuildEclipseCompiler SetProperties'
     //powershell 'Write-Output "Hello, World!"'
-    bat "\"${tool 'MSBuild-Default'}\" -Version"
+    bat "\"${tool 'MSBuild'}\" -Version"
 }
