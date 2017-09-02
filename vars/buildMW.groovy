7ba895955397cf88febefb008fba52d766acb54c
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-02 20:08:51 +0300

    Implement Build step in mw-build pipeline

--- vars/buildMW.groovy
+++ vars/buildMW.groovy
@@ -4,7 +4,24 @@ def call() {
     bat 'ant BuildEclipseCompiler SetProperties'
     //powershell 'Write-Output "Hello, World!"'
     def stdout = powershell(returnStdout: true, script: '''
-        Write-Output 'Hello, World!'
+        $path = $ENV:WORKSPACE
+        $year = Get-Date -Format yyyy
+        $month = Get-Date -Format MM
+
+        $text = "using System;
+        using System.Reflection;
+
+        [assembly: AssemblyVersion(`"6.52.$year.$month`")]
+        [assembly: AssemblyFileVersion(`"6.52.$year.$month`")]
+
+        [assembly: AssemblyCompany(`"Kronos Inc`")]
+        [assembly: AssemblyProduct(`"Kronos Workforce Ready`")]
+        [assembly: AssemblyCopyright(`"Copyright (c) 1996-$year Kronos Inc`")]
+        [assembly: CLSCompliant(true)]"
+
+        Write-Output $text
+        $text | Set-Content "$path\\src\\AccessControl\\Global\\GlobalAssemblyInfo.cs"
+
     ''')
     println stdout
 
