7e257ce99283566e058c605bf1ec7e8145d05b9f
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-04 18:01:57 +0300

    Implement Build step in mw-build pipeline

--- vars/publishMW.groovy
+++ vars/publishMW.groovy
@@ -3,15 +3,15 @@
 def call(String repo) {
     powershell '''
         .\\zip.cmd
-        $mwSource = 'c:\MW'
+        $mwSource = 'c:\\MW'
         $mwZipPath = $env:MiddlewarePath + 'MW.zip'
         $mwExePath = $env:MiddlewarePath + 'MW.ex_'
 
         Write-Host 'Path is: '$mwZipPath
 
-        CScript .\zip.vbs $mwSource 'c:\MW.zip'
+        CScript .\\zip.vbs $mwSource 'c:\\MW.zip'
 
-        Copy-Item c:\MW.zip -Destination $mwZipPath -Force
-        Copy-Item c:\MW.zip -Destination $mwExePath -Force
+        Copy-Item c:\\MW.zip -Destination $mwZipPath -Force
+        Copy-Item c:\\MW.zip -Destination $mwExePath -Force
     '''
 }
