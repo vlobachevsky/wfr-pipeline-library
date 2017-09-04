322f785ce204a1250f757d5f394079b464d03177
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-04 10:22:29 +0300

    Implement Build step in mw-build pipeline

---
+++ vars/buildPlugin.groovy
@@ -0,0 +1,13 @@
+// vars/buildPlugin.groovy
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    // now build, based on the configuration provided
+    node {
+        echo "Hello, ${name}."
+    }
+}
