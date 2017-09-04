1672a9550e537548ac5ed968e21eb8ecc8a1e1e6
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-04 11:50:44 +0300

    Implement Build step in mw-build pipeline

--- vars/checkoutSVN.groovy
+++ vars/checkoutSVN.groovy
@@ -2,34 +2,6 @@
 
 // vars/checkoutSVN.groovy
 
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
-    def credentialsId = config.credentialsId
-    def url = config.url
-    def localDir = config.localDir ?: '.'
-    def depthOption = conif.depthOption ?: 'infinity'
-
-/*
-    checkout([
-      $class: 'SubversionSCM',
-      locations: [[
-        credentialsId: "${credentialsId}",
-        depthOption: "${depthOption}",
-        ignoreExternalsOption: true,
-        local: "${localDir}",
-        remote: "${url}"
-      ]],
-      workspaceUpdater: [$class: 'UpdateWithRevertUpdater']
-    ])
-*/    
-}
-
-/*
 def call(String credentialsId, String url, String localDir = '.', String depthOption = 'infinity') {
     checkout([
       $class: 'SubversionSCM',
@@ -43,4 +15,3 @@ def call(String credentialsId, String url, String localDir = '.', String depthOp
       workspaceUpdater: [$class: 'UpdateWithRevertUpdater']
     ])
 }
-*/
