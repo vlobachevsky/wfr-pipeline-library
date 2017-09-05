173824aa6db4622d20ec622959c51dcf6f76fb18
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-05 12:20:11 +0300

    Implement start/stop MW in mw-build pipeline

---
+++ vars/startMW.groovy
@@ -0,0 +1,5 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo 'startMW() is not implemented yet'
+}
---
+++ vars/stopMW.groovy
@@ -0,0 +1,5 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo 'stopMW() is not implemented yet'
+}
