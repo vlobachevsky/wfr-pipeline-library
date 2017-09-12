5daa101261402119dd5a291ca90d2606d9cbdb7a
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-12 14:22:24 +0300

    Fix exit status

--- vars/publishMW.groovy
+++ vars/publishMW.groovy
@@ -24,8 +24,18 @@ def call(Map params = [:]) {
 */
     def status = powershell(returnStatus: true, script: """
         CD .\\PunchMW
-        .\\zip2.cmd
+        .\\zip.cmd
+        \$mwSource = 'c:\\MW'
+        \$mwZipPath = '${repo}' + 'MW.zip'
+        \$mwExePath = '${repo}' + 'MW.ex_'
+
+        Write-Output 'Path is: '\$mwZipPath
+        
+        CScript .\\zip.vbs \$mwSource 'c:\\MW.zip'
+
+        Copy-Item c:\\MW.zip -Destination \$mwZipPath -Force
         Copy-Item c:\\MW.zip -Destination \$mwExePath -Force
+
         exit \$LastExitCode
     """)
 
