35af984cb90e316a7192c908aa3cac3cc36d3cfa
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-12 14:56:44 +0300

    Fix exit status

--- vars/publishMW.groovy
+++ vars/publishMW.groovy
@@ -36,12 +36,12 @@ def call(Map params = [:]) {
         if (Test-Path \$mwZipPath) {
             Copy-Item c:\\MW.zip -Destination \$mwZipPath -Force
         } else {
-            Write-Error 'Can't find path \$mwZipPath'
+            Write-Output 'Can't find path \$mwZipPath'
         }
         if (Test-Path \$mwExePath) {
             Copy-Item c:\\MW.zip -Destination \$mwExePath -Force
         } else {
-            Write-Error 'Can't find path \$mwExePath'
+            Write-Output 'Can't find path \$mwExePath'
         }
         exit \$LastExitCode
     """)
