a63787d67f80ad15b6400422b22eaee6c213d01a
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-12 12:52:45 +0300

    Fix exit status

--- vars/publishMW.groovy
+++ vars/publishMW.groovy
@@ -19,6 +19,7 @@ def call(Map params = [:]) {
         Copy-Item c:\\MW.zip -Destination \$mwZipPath -Force
         Copy-Item c:\\MW.zip -Destination \$mwExePath -Force
     """)
+    echo "Status: ${status}"
     if (status != 0) {
         powershell 'exit $LastExitCode'
     }
