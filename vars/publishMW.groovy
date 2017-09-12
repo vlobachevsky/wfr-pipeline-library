341c688c989862fe1753151c695ca09dfd7dbbd2
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-12 17:14:19 +0300

    Fix exit status

--- vars/publishMW.groovy
+++ vars/publishMW.groovy
@@ -5,61 +5,6 @@
 def call(Map params = [:]) {
     def repo = params.repo
 
-/*
-    def status = powershell(returnStatus: true, script: """
-        CD .\\PunchMW
-        .\\zip.cmd
-        \$mwSource = 'c:\\MW'
-        \$mwZipPath = '${repo}' + 'MW.zip'
-        \$mwExePath = '${repo}' + 'MW.ex_'
-
-        Write-Output 'Path is: '\$mwZipPath
-
-        CScript .\\zip.vbs \$mwSource 'c:\\MW.zip'
-
-        Copy-Item c:\\MW.zip -Destination \$mwZipPath -Force
-        Copy-Item c:\\MW.zip -Destination \$mwExePath -Force
-        exit 1
-    """)
-*/
-
-/*
-    def status = powershell(returnStatus: true, script: """
-        if (Test-Path '${repo}') {
-            Write-Output 'Cannot find path: ${repo}'
-            Exit
-        }
-        Write-Output 'Sanity Check'
-
-        CD .\\PunchMW
-        .\\zip.cmd
-        \$mwSource = 'c:\\MW'
-        \$mwZipPath = '${repo}' + 'MW.zip'
-        \$mwExePath = '${repo}' + 'MW.ex_'
-
-        Write-Output 'Path is: '\$mwZipPath
-
-        CScript .\\zip.vbs \$mwSource 'c:\\MW.zip'
-
-        Copy-Item c:\\MW.zip -Destination \$mwZipPath -Force
-        Copy-Item c:\\MW.zip -Destination \$mwExePath -Force
-
-        if (Test-Path \$mwZipPath) {
-            Copy-Item c:\\MW.zip -Destination \$mwZipPath -Force
-        } else {
-            Write-Output 'Cannot find path: '\$mwZipPath
-            exit 1
-        }
-        if (Test-Path \$mwExePath) {
-            Copy-Item c:\\MW.zip -Destination \$mwExePath -Force
-        } else {
-            Write-Output 'Cannot find path: '\$mwExePath
-            exit 1
-        }
-        exit \$LastExitCode
-    """)
-*/
-
     def status = powershell(returnStatus: true, script: """
         if ( !(Test-Path '${repo}') ) {
             Write-Output 'Cannot find path: ${repo}'
