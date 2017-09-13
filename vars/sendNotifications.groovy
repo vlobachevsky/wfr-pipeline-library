f694c4d4826915643b0aa6cbe185130bbcc40f3d
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-13 21:16:52 +0300

    Implement Send Notifications

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -2,7 +2,7 @@
 
 def call(Map params = [:]) {
     def to = params.to ?: 'Vital.Lobachevskij@Kronos.com'
-    def subject = params.subject ?: "${env.PROJECT_NAME} - Build # ${env.BUILD_NUMBER} - ${env.BUILD_STATUS}!"
+    def subject = params.subject ?: "${env.JOB_NAME} - Build # ${env.BUILD_NUMBER} - ${env.BUILD_STATUS}!"
     def body = params.body ?: "$env.DEFAULT_CONTENT"
 
     emailext(
