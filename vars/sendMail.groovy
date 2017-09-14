ab6c734d6dba5a5fe44f266a2db607c9719e3ffa
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-14 14:27:50 +0300

    Implement Send Notifications

--- vars/sendMail.groovy
+++ vars/sendMail.groovy
@@ -1,23 +1,22 @@
-// vars/sendMail.groovy
+#!/usr/bin/env groovy
+
 /**
- * sendMail.groovy
+ * vars/sendMail.groovy
  *
  * This function uses emailext (https://wiki.jenkins.io/display/JENKINS/Email-ext+plugin) to send an email
- * - An email will always be send to the requestor (the one who started the job)
- * - In case of a failure the email will be send to the curlprits
+ * - Email is sent in html format
  *
- * @param to            [null|string]												The email recipients
- * @param subject		["<JOB_NAME> - Build #<BUILD_NUMBER> - <result>!" | string] The email subject
- * @param body  		[null|true|false]											By default the buildlog will be included when the build fails
- * @param attachLog		[null|true|false]											By default the buildlog will be included when the build fails
- *
+ * @param to            [env.PROJECT_RECIPIENT_LIST | string]							        The email recipients
+ * @param subject		["$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!" | string]     The email subject
+ * @param body  		[$DEFAULT_CONTENT | string]         	  								The email body
+ * @param attachLog		[true | false]  											            By default the buildlog will be included when the build fails
  */
-
 def call(Map params = [:]) {
     def to = params.to ?: "${env.PROJECT_RECIPIENT_LIST}"
     def subject = params.subject ?: '${PROJECT_NAME} - Build # ${BUILD_NUMBER} - ${BUILD_STATUS}!'
     def body = params.body ?: '${DEFAULT_CONTENT}'
-    def attachLog = params.attachLog ?: (currentBuild.currentResult != "SUCCESS") // Attach buildlog when the build is not successfull
+    // Attach buildlog when the build is not successfull
+    def attachLog = params.attachLog ?: (currentBuild.currentResult != "SUCCESS")
 
     // Send email
     emailext(
