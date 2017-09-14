f06168179b87d5357c0f6cd0760864e0d583a97c
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-14 11:12:14 +0300

    Implement Send Notifications

--- vars/sendMail.groovy
+++ vars/sendMail.groovy
@@ -13,6 +13,8 @@
  */
 
 def call(Map params = [:]) {
+    echo 'DEFAULT_REPLYTO: $DEFAULT_REPLYTO'
+
     def subject = params.subject ? params.subject : "${env.JOB_NAME} - Build #${env.BUILD_NUMBER} - ${currentBuild.result}!"
     def content = '${JELLY_SCRIPT,template="static-analysis"}'
     def attachLog = (params.attachLog != null) ? params.attachLog : (currentBuild.result != "SUCCESS") // Attach buildlog when the build is not successfull
