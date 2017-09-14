640946edd802d4c591a833df935a8ab402dd744a
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-14 12:05:19 +0300

    Implement Send Notifications

--- vars/sendMail.groovy
+++ vars/sendMail.groovy
@@ -13,8 +13,8 @@
  */
 
 def call(Map params = [:]) {
-    def buildResult = currentBuild.result
-    echo "Current Build Result: ${buildResult}"
+    def buildStatus = params.buildStatus
+    echo "Current Build Result: ${buildStatus}"
 
     def subject = params.subject ?: '${PROJECT_NAME} - Build # ${BUILD_NUMBER} - ${BUILD_STATUS}!'
 
