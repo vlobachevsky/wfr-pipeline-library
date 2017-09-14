1c513a1797796edbc2c64a2063c6dc7c85079d8b
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-14 09:30:53 +0300

    Implement Send Notifications

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -1,5 +1,14 @@
 #!/usr/bin/env groovy
 
+/**
+ * vars/sendNotifications.groovy
+ *
+ * This function uses emailtext to send an email.
+ * @param emailRecipients	[null|string]												The email recipients
+ * @param subject			["<JOB_NAME> - Build #<BUILD_NUMBER> - <result>!" | string] The email subject
+ * @param attachLog			[null|true|false]											By default the buildlog will be included when the build fails
+ */
+
 def call(Map params = [:]) {
     def to = params.to ?: 'Vital.Lobachevskij@Kronos.com'
     def subject = params.subject ?: "${env.JOB_NAME} - Build # ${env.BUILD_NUMBER} - ${env.BUILD_STATUS}!"
