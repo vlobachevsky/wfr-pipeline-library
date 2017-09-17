b3a733269e547f7212fae875d0ea748d19321e50
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-17 21:29:40 +0300

    Implement new publish Client MW step

--- vars/publishClientMW.groovy
+++ vars/publishClientMW.groovy
@@ -3,7 +3,7 @@
 def call(Map params = [:]) {
     assert params.repo
 
-    def path = params.repo.replaceAll('\\', '\\\\')
+    def path = params.repo.replace("\\", "\\\\")
     def propertyfile = 'build_client_mw.properties'
     writeFile file: "${propertyfile}", text: "client.release.dir=${path}"
 //    bat "ant -propertyfile='${propertyfile}' PackageClientMW"
